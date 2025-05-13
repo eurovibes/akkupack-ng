3973e52a5ead21619df7b0dd6994f182381f745d
Benedikt Spranger <b.spranger@linutronix.de>
2025-05-13 14:39:10 +0200

    WIP
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>

--- software/rust/akkupack-ng/src/lib.rs
+++ software/rust/akkupack-ng/src/lib.rs
@@ -11,18 +11,6 @@ use embassy_stm32::time::Hertz;
 use embassy_stm32::Peripherals;
 use {defmt_rtt as _, panic_probe as _};
 
-const AI: [u8; 1] = [0x0a];
-const CNTL: u8 = 0x00;
-const FCC: [u8; 1] = [0x06];
-const FLAGS: [u8; 1] = [0x0e];
-const FLAGSB: [u8; 1] = [0x12];
-const I: [u8; 1] = [0x10];
-const ME: [u8; 1] = [0x03];
-const RM: [u8; 1] = [0x04];
-const SOC: [u8; 1] = [0x02];
-const TEMP: [u8; 1] = [0x0c];
-const VOLT: [u8; 1] = [0x04];
-
 macro_rules! def_enum {
     (
         $(#[$attr:meta])*
@@ -48,29 +36,78 @@ macro_rules! def_enum {
     };
 }
 
+def_enum!(
+    #[derive(PartialEq,Eq, Debug)]
+    pub Cmd => u8 {
+        CMD_AI => 0x0a,
+        CMD_CNTL => 0x00,
+        CMD_FCC => 0x06,
+        CMD_FLAGS => 0x0e,
+        CMD_FLAGSB => 0x12,
+        CMD_I => 0x10,
+        CMD_ME => 0x03,
+        CMD_RM => 0x04,
+        CMD_SOC => 0x02,
+        CMD_TEMP => 0x0c,
+        CMD_VOLT => 0x04,
+        CMD_ATTE => 0x18,
+        CMD_ATTF => 0x1A,
+        CMD_PCHG => 0x1C,
+        CMD_DOD0T => 0x1E,
+        CMD_AE => 0x24,
+        CMD_AP => 0x26,
+        CMD_SERNUM => 0x28,
+        CMD_INTTEMP => 0x2A,
+        CMD_CC => 0x2C,
+        CMD_SOH => 0x2E,
+        CMD_CHGV => 0x30,
+        CMD_CHGI => 0x32,
+        CMD_PKCFG => 0x3A,
+        CMD_DCAP => 0x3C,
+        CMD_DFCLS => 0x3E,
+        CMD_DFBLK => 0x3F,
+        CMD_A_DF => 0x40,
+        CMD_ACKS_DFD => 0x54,
+        CMD_DFD => 0x55,
+        CMD_DFDCKS => 0x60,
+        CMD_DFDCNTL => 0x61,
+        CMD_GN => 0x62,
+        CMD_LS => 0x63,
+        CMD_DEOC => 0x64,
+        CMD_QS => 0x66,
+        CMD_TRC => 0x68,
+        CMD_TFCC => 0x6A,
+        CMD_ST => 0x6C,
+        CMD_QPC => 0x6E,
+        CMD_DOD0 => 0x70,
+        CMD_QD0 => 0x72,
+        CMD_QT => 0x74,
+    }
+);
+
 /*
 Gauge control subcommands:
-CONTROL_STATUS 0x0000 Yes Reports the status of key features.
-DEVICE_TYPE 0x0001 Yes Reports the device type of 0x100 (indicating BQ34Z100-G1)
-FW_VERSION 0x0002 Yes Reports the firmware version on the device type
-HW_VERSION 0x0003 Yes Reports the hardware version of the device type
-RESET_DATA 0x0005 Yes Returns reset data
-PREV_MACWRITE 0x0007 Yes Returns previous Control() command code
-CHEM_ID 0x0008 Yes Reports the chemical identifier of the Impedance Track configuration
-BOARD_OFFSET 0x0009 Yes Forces the device to measure and store the board offset
-CC_OFFSET 0x000A Yes Forces the device to measure the internal CC offset
-CNTL FUNCTION CNTL DATA SEALED ACCESS DESCRIPTION
-CC_OFFSET_SAVE 0x000B Yes Forces the device to store the internal CC offset
-DF_VERSION 0x000C Yes Reports the data flash version on the device
-SET_FULLSLEEP 0x0010 Yes Set the [FULLSLEEP] bit in the control register to 1
+Control function   Subcmd Sealed access Description 
+CONTROL_STATUS     0x0000 Yes Reports the status of key features.
+DEVICE_TYPE        0x0001 Yes Reports the device type of 0x100 (indicating BQ34Z100-G1)
+FW_VERSION         0x0002 Yes Reports the firmware version on the device type
+HW_VERSION         0x0003 Yes Reports the hardware version of the device type
+RESET_DATA         0x0005 Yes Returns reset data
+PREV_MACWRITE      0x0007 Yes Returns previous Control() command code
+CHEM_ID            0x0008 Yes Reports the chemical identifier of the Impedance Track configuration
+BOARD_OFFSET       0x0009 Yes Forces the device to measure and store the board offset
+CC_OFFSET          0x000A Yes Forces the device to measure the internal CC offset
+CC_OFFSET_SAVE     0x000B Yes Forces the device to store the internal CC offset
+DF_VERSION         0x000C Yes Reports the data flash version on the device
+SET_FULLSLEEP      0x0010 Yes Set the [FULLSLEEP] bit in the control register to 1
 STATIC_CHEM_CHKSUM 0x0017 Yes Calculates chemistry checksum
-SEALED 0x0020 No Places the device in SEALED access mode
-IT_ENABLE 0x0021 No Enables the Impedance Track algorithm
-CAL_ENABLE 0x002D No Toggle CALIBRATION mode enable
-RESET 0x0041 No Forces a full reset of the BQ34Z100-G1
-EXIT_CAL 0x0080 No Exit CALIBRATION mode
-ENTER_CAL 0x0081 No Enter CALIBRATION mode
-OFFSET_CAL 0x0082 No Reports internal CC offset in CALIBRATION mod
+SEALED             0x0020 No  Places the device in SEALED access mode
+IT_ENABLE          0x0021 No  Enables the Impedance Track algorithm
+CAL_ENABLE         0x002D No  Toggle CALIBRATION mode enable
+RESET              0x0041 No  Forces a full reset of the BQ34Z100-G1
+EXIT_CAL           0x0080 No  Exit CALIBRATION mode
+ENTER_CAL          0x0081 No  Enter CALIBRATION mode
+OFFSET_CAL         0x0082 No  Reports internal CC offset in CALIBRATION mod
 */
 
 def_enum!(
@@ -100,16 +137,17 @@ def_enum!(
 );
 
 pub trait Gauge {
-    fn cntrl(&mut self, subcmd: SubCmds) -> u16;
-    fn get_cap_full(&mut self) -> u16;
-    fn get_cap_remain(&mut self) -> u16;
-    fn get_current(&mut self) -> u16;
-    fn get_current_avg(&mut self) -> u16;
-    fn get_flags(&mut self) -> u32;
-    fn get_max_error(&mut self) -> u8;
-    fn get_state_of_charge(&mut self) -> u8;
-    fn get_temp(&mut self) -> u16;
-    fn get_voltage(&mut self) -> u16;
+    fn cntrl(&mut self, subcmd: SubCmds) -> Result<u16, Error>;
+    fn get_cap_full(&mut self) -> Result<u16, Error>;
+    fn get_cap_remain(&mut self) -> Result<u16, Error>;
+    fn get_current(&mut self) -> Result<u16, Error>;
+    fn get_current_avg(&mut self) -> Result<u16, Error>;
+    fn get_flags(&mut self) -> Result<u32, Error>;
+    fn get_max_error(&mut self) -> Result<u8, Error>;
+    fn get_state_of_charge(&mut self) -> Result<u8, Error>;
+    fn get_temp(&mut self) -> Result<u16, Error>;
+    fn get_voltage(&mut self) -> Result<u16, Error>;
+    fn read_data_flash_class(&mut self, class: u8, data: &mut [u8]) -> Result<(), Error>;
 }
 
 pub struct BqXxx<'a> {
@@ -126,88 +164,170 @@ impl<'a> BqXxx<'a> {
         this
     }
 
-    fn read(&mut self, cmd: [u8; 1]) -> u16 {
+    fn read(&mut self, cmd: Cmd) -> Result<u16, Error> {
         let mut data = [0u8; 2];
+        let c = cmd.get();
 
-        match self.i2c.blocking_write_read(self.addr, &cmd, &mut data) {
-            Ok(()) => trace!("voltage(): {}", (data[0] as u16) << 8 | data[1] as u16),
+        match self.i2c.blocking_write_read(self.addr, &[c], &mut data) {
+            Ok(()) => trace!("cmd({}): {}", c, (data[1] as u16) << 8 | data[0] as u16),
+            Err(Error::Timeout) => {
+                error!("Operation timed out");
+                return Err(Error::Timeout);
+            },
+            Err(e) => {
+                error!("I2c Error: {:?}", e);
+                return Err(e);
+            },
+        }
+
+        let result = ((data[1] as u16) << 8) | data[0] as u16;
+        Ok(result)
+    }
+
+    pub fn dev_type(&mut self) -> u16 {
+        let mut rx_buffer: [u8; 2] = [0; 2];
+
+        match self.i2c.blocking_write(0x55, &[0, 1, 0]) {
+            Ok(()) => debug!("Successful command write"),
             Err(Error::Timeout) => error!("Operation timed out"),
             Err(e) => error!("I2c Error: {:?}", e),
         }
 
-        let result = ((data[0] as u16) << 8) | data[1] as u16;
-        result
+        match self.i2c.blocking_read(self.addr, &mut rx_buffer) {
+            Ok(()) => debug!("read(): {:#04x} {:#04x}", rx_buffer[0], rx_buffer[1]),
+            Err(Error::Timeout) => error!("Operation timed out"),
+            Err(e) => error!("I2c Error: {:?}", e),
+        }
+
+        ((rx_buffer[1] as u16) << 8) | rx_buffer[0] as u16
     }
 }
 
 impl Gauge for BqXxx<'_> {
-    fn cntrl(&mut self, subcmd: SubCmds) -> u16 {
+    fn cntrl(&mut self, subcmd: SubCmds) -> Result<u16, Error> {
+        let c = Cmd::CMD_CNTL.get();
         let sc: u16 = subcmd.get();
-        let cmd = [CNTL, sc as u8, (sc >> 8) as u8, CNTL];
+        let cmd = [c, sc as u8, (sc >> 8) as u8];
         let mut data = [0u8; 2];
 
         match self.i2c.blocking_write_read(self.addr, &cmd, &mut data) {
-            Ok(()) => trace!("cntrl({}): {}", sc, (data[0] as u16) << 8 | data[1] as u16),
-            Err(Error::Timeout) => error!("Operation timed out"),
-            Err(e) => error!("I2c Error: {:?}", e),
+            Ok(()) => debug!("cntrl({}): {:#06x}", sc, (data[1] as u16) << 8 | data[0] as u16),
+            Err(Error::Timeout) => {
+                error!("Operation timed out");
+                return Err(Error::Timeout);
+            },
+            Err(e) => {
+                error!("I2c Error: {:?}", e);
+                return Err(e);
+            },
         }
 
-        ((data[0] as u16) << 8) | data[1] as u16
+        Ok(((data[1] as u16) << 8) | data[0] as u16)
     }
 
-    fn get_cap_full(&mut self) -> u16 {
-        self.read(FCC)
+    fn get_cap_full(&mut self) -> Result<u16, Error> {
+        self.read(Cmd::CMD_FCC)
     }
 
-    fn get_cap_remain(&mut self) -> u16 {
-        self.read(RM)
+    fn get_cap_remain(&mut self) -> Result<u16, Error> {
+        self.read(Cmd::CMD_RM)
     }
 
-    fn get_current(&mut self) -> u16 {
-        self.read(I)
+    fn get_current(&mut self) -> Result<u16, Error> {
+        self.read(Cmd::CMD_I)
     }
 
-    fn get_current_avg(&mut self) -> u16 {
-        self.read(AI)
+    fn get_current_avg(&mut self) -> Result<u16, Error> {
+        self.read(Cmd::CMD_AI)
     }
 
-    fn get_flags(&mut self) -> u32 {
-        let flags = self.read(FLAGS);
-        let flagsb = self.read(FLAGSB);
+    fn get_flags(&mut self) -> Result<u32, Error> {
+        let flags = match self.read(Cmd::CMD_FLAGS) {
+            Ok(flags) => flags,
+            Err(e) => return Err(e),
+        };
+        let flagsb = match self.read(Cmd::CMD_FLAGSB) {
+            Ok(flagsb) => flagsb,
+            Err(e) => return Err(e),
+        };
 
-        ((flags as u32) << 16) | flagsb as u32
+        Ok(((flagsb as u32) << 16) | flags as u32)
     }
 
-    fn get_max_error(&mut self) -> u8 {
+    fn get_max_error(&mut self) -> Result<u8, Error> {
+        let mut data = [0u8; 1];
+
+        match self.i2c.blocking_write(self.addr, &[Cmd::CMD_ME.get()]) {
+            Ok(()) => trace!("write OK!"),
+            Err(Error::Timeout) => {
+                error!("Operation timed out");
+                return Err(Error::Timeout);
+            },
+            Err(e) => {
+                error!("I2c Error: {:?}", e);
+                return Err(e);
+            },
+        }
+
+        match self
+            .i2c
+            .blocking_write_read(self.addr, &[Cmd::CMD_ME.get()], &mut data)
+        {
+            Ok(()) => trace!("MaxError: {}", data[0]),
+            Err(Error::Timeout) => {
+                error!("Operation timed out");
+                return Err(Error::Timeout);
+            },
+            Err(e) => {
+                error!("I2c Error: {:?}", e);
+                return Err(e);
+            },
+        }
+        Ok(data[0])
+    }
+
+    fn get_state_of_charge(&mut self) -> Result<u8, Error> {
         let mut data = [0u8; 1];
 
         match self
             .i2c
-            .blocking_write_read(self.addr, &ME, &mut data)
+            .blocking_write_read(self.addr, &[Cmd::CMD_SOC.get()], &mut data)
         {
-            Ok(()) => trace!("MaxError: {}", data[0]),
-            Err(Error::Timeout) => error!("Operation timed out"),
-            Err(e) => error!("I2c Error: {:?}", e),
-        }
-        data[0]
-    }
-
-    fn get_state_of_charge(&mut self) -> u8 {
-        let mut data = [0u8; 1];
-
-        match self.i2c.blocking_write_read(self.addr, &SOC, &mut data) {
             Ok(()) => trace!("StateOfCharge: {}", data[0]),
-            Err(Error::Timeout) => error!("Operation timed out"),
-            Err(e) => error!("I2c Error: {:?}", e),
+            Err(Error::Timeout) => {
+                error!("Operation timed out");
+                return Err(Error::Timeout);
+            },
+            Err(e) => {
+                error!("I2c Error: {:?}", e);
+                return Err(e);
+            },
         }
-        data[0]
+        Ok(data[0])
     }
 
-    fn get_temp(&mut self) -> u16 {
-        self.read(TEMP)
+    fn get_temp(&mut self) -> Result<u16, Error> {
+        self.read(Cmd::CMD_TEMP)
     }
 
-    fn get_voltage(&mut self) -> u16 {
-        self.read(VOLT)
+    fn get_voltage(&mut self) -> Result<u16, Error> {
+        self.read(Cmd::CMD_VOLT)
+    }
+
+    fn read_data_flash_class(&mut self, class: u8, data: &mut [u8]) -> Result<(), Error> {
+        let mut start: usize = 0;
+        let mut block: usize = 0;
+        let mut len: usize;
+
+        loop {
+            len = data.len() - start;
+            if (len > 32) {
+                len = 32;
+            }
+
+
+        }
+
+        Ok(())
     }
 }
--- software/rust/akkupack-ng/src/main.rs
+++ software/rust/akkupack-ng/src/main.rs
@@ -7,10 +7,6 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::rcc::{
-    AHBPrescaler, APBPrescaler, Hse, HseMode, Pll, PllPreDiv, PllSource, Sysclk,
-};
-use embassy_stm32::time::Hertz;
 use embassy_stm32::Config;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -21,29 +17,48 @@ const ADDRESS: u8 = 0x55;
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Hello BQ34Z100-G1!");
-    let mut config = Config::default();
-    {
-        config.rcc.hse = Some(Hse {
-            freq: Hertz(8_000_000),
-            mode: HseMode::Oscillator,
-        });
-        config.rcc.pll = Some(Pll {
-            src: PllSource::HSE,
-            prediv: PllPreDiv::DIV1,
-            mul: embassy_stm32::rcc::PllMul::MUL9,
-        });
-        config.rcc.ahb_pre = AHBPrescaler::DIV1;
-        config.rcc.apb1_pre = APBPrescaler::DIV2;
-        config.rcc.apb2_pre = APBPrescaler::DIV1;
-        config.rcc.sys = Sysclk::PLL1_P;
-    }
+    let config = Config::default();
     let p = embassy_stm32::init(config);
 
     let mut bq = BqXxx::new(p, ADDRESS);
 
+    info!("Device type    : {}", bq.dev_type());
+
     info!("MaxError       : {} %", bq.get_max_error());
     info!("State of charge: {} %", bq.get_state_of_charge());
-    info!("FW version     : {}", bq.cntrl(SubCmds::SUB_CMD_FW_VERSION));
+    info!("Device type    : {}", bq.cntrl(SubCmds::SUB_CMD_DEVICE_TYPE));
+    info!("FW version     : {:#06x}", bq.cntrl(SubCmds::SUB_CMD_FW_VERSION));
+    info!("HW version     : {}", bq.cntrl(SubCmds::SUB_CMD_HW_VERSION));
+    info!("DF version     : {}", bq.cntrl(SubCmds::SUB_CMD_DF_VERSION));
     info!("Voltage        : {} V", bq.get_voltage());
+    info!("Full capacity  : {}", bq.get_cap_full());
+    info!("Rem. capacity  : {}", bq.get_cap_remain());
+    info!("Current        : {}", bq.get_current());
+    info!("Current Avg    : {}", bq.get_current_avg());
+    info!("Flags          : {:#010x}", bq.get_flags());
+    info!("Temperature    : {}", bq.get_temp());
     info!("Control status : {:#06x}", bq.cntrl(SubCmds::SUB_CMD_CONTROL_STATUS));
+
+    // gauge_execute_fs(pHandle, pFileBuffer)
+
+    //read data class DC_STATE:
+    // n = gauge_read_data_class(pHandle, DC_STATE, pData, DC_STATE_LENGTH);
+    // if (n) printf("Error reading data class, %d\n\r", n);
+    // printf("Data Class 'State' (0x52):\n\r");
+    // print_data(pData, DC_STATE_LENGTH);
+    // this was for bq2742x - change offsets for your gauge
+    // pData[10] = (DESIGN_CAPACITY & 0xFF00) >> 8;
+    // pData[11] = DESIGN_CAPACITY & 0xFF;
+    // pData[12] = (DESIGN_ENERGY & 0xFF00) >> 8;
+    // pData[13] = DESIGN_ENERGY & 0xFF;
+    // pData[16] = (TERMINATE_VOLTAGE & 0xFF00) >>8;
+    // pData[17] = TERMINATE_VOLTAGE & 0xFF;
+    // pData[27] = (TAPER_RATE & 0xFF00) >> 8;
+    // pData[28] = TAPER_RATE & 0xFF;
+    //write data class DC_STATE:
+    // gauge_cfg_update(pHandle);
+    // n = gauge_write_data_class(pHandle, DC_STATE, pData, DC_STATE_LENGTH);
+    // if (n) printf("Error writing data class, %d\n\r", n);
+    // gauge_exit(pHandle, SOFT_RESET);
+
 }
